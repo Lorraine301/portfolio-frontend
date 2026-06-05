496cb190ac761a2cc5d57b16dafbd8eb85809773
Lorraine301 <andriamasylorraine@gmail.com>
2026-06-05 10:22:05 +0100

    Update location in HeroSection from 'Tanger' to 'Maroc' for accuracy

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -62,7 +62,7 @@
           <svg class="inline-block mr-1.5 align-middle w-3.5 h-3.5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
           </svg>
-          Tanger
+          Maroc
         </div>
       </div>
     </div>
